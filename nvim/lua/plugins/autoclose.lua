55d8ed571831039bec821815b7137718fa7d53ed
xoldv <the507015t@gmail.com>
2025-04-21 23:33:24 +0200

    feat(nvim): change autoclose to nvim-autopairs

--- nvim/lua/plugins/autoclose.lua
+++
@@ -1,7 +0,0 @@
--- autoclose brackets
-return {
-   'm4xshen/autoclose.nvim',
-    config = function ()
-    require("autoclose").setup()
-    end,
-}
---
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+-- autoclose brackets
+return {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
