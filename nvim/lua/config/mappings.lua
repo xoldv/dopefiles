08fc4cced1d26d01b97e36f3dc9252c8cd517066
xoldv <the507015t@gmail.com>
2025-04-22 11:16:20 +0200

    feat: change gruvbox theme, add day/night toggle

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -4,38 +4,40 @@ vim.g.mapleader = " "
 -- vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 
 -- Navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>")
 
 -- Splits
-vim.keymap.set('n', '_', ':vsplit<CR>')
-vim.keymap.set('n', '-', ':split<CR>')
+vim.keymap.set("n", "_", ":vsplit<CR>")
+vim.keymap.set("n", "-", ":split<CR>")
 
 -- Other
-vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
-vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
-vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
-vim.keymap.set('n', '<leader>z', ':BufferLineCloseOthers<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<leader>X", ":BufferLineCloseRight<CR>")
+vim.keymap.set("n", "<leader>s", ":BufferLineSortByTabs<CR>")
+vim.keymap.set("n", "<leader>z", ":BufferLineCloseOthers<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
 -- Tabs
-vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
 
 -- Debug ( DAP )
-vim.keymap.set('n', '<Leader>dt', ':DapToggleBreakpoint<CR>')
-vim.keymap.set('n', '<Leader>dc', ':DapContinue<CR>')
-vim.keymap.set('n', '<F2>', ':DapStepOver<CR>')
-vim.keymap.set('n', '<F3>', ':DapStepInto<CR>')
-vim.keymap.set('n', '<F4>', ':DapStepOut<CR>')
+vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+vim.keymap.set("n", "<F2>", ":DapStepOver<CR>")
+vim.keymap.set("n", "<F3>", ":DapStepInto<CR>")
+vim.keymap.set("n", "<F4>", ":DapStepOut<CR>")
 
 -- Go to declaration
-vim.api.nvim_set_keymap('n', 'gd', '<C-]>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<C-]>", { noremap = true, silent = true })
 
 -- fasting change name
-vim.keymap.set('n', 'ms', '*:%s//')
+vim.keymap.set("n", "ms", "*:%s//")
+
+vim.keymap.set("n", "<C-y>", "<C-d>", { noremap = true })
--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,21 +1,47 @@
+-- set italic in treesitter for more readable
+local function set_italic(group)
+	local current = vim.api.nvim_get_hl(0, { name = group, link = false }) or {}
+	current.italic = true
+	vim.api.nvim_set_hl(0, group, current)
+end
+
+local function apply_italic_highlights()
+	set_italic("@keyword")
+	set_italic("@function.method.call")
+	set_italic("@function.call")
+end
+
+-- for fast change light and dark theme
+--
+vim.api.nvim_create_user_command("ToggleBackground", function()
+	if vim.o.background == "light" then
+		vim.cmd("set background=dark")
+	else
+		vim.cmd("set background=light")
+	end
+
+	vim.cmd.colorscheme("gruvbox-material")
+	vim.api.nvim_set_hl(0, "CursorLine", { bg = vim.o.background == "light" and "#dbddeb" or "#333852" })
+	vim.api.nvim_set_hl(0, "CursorColumn", { bg = vim.o.background == "light" and "#dbddeb" or "#333852" })
+	vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = vim.o.background == "light" and "#ffffff" or "#504945" })
+	vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = vim.o.background == "light" and "#ffffff" or "#504945" })
+	vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = vim.o.background == "light" and "#ffffff" or "#504945" })
+	apply_italic_highlights()
+end, { desc = "Toggle between light and dark background with italics" })
+
+-- keybind for fast change
+vim.keymap.set("n", "<M-t>", ":ToggleBackground<CR>", { noremap = true })
+
 return {
-	"eddyekofo94/gruvbox-flat.nvim",
+	"sainnhe/gruvbox-material",
+	lazy = false,
 	priority = 1000,
-	enabled = true,
 	config = function()
-
-		vim.g.gruvbox_flat_style = "dark"
+		vim.g.gruvbox_material_enable_italic = 1
 		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "gruvbox-flat",
+			pattern = "gruvbox-material",
 			callback = function()
-
-				vim.opt.conceallevel = 1
 				vim.api.nvim_set_hl(0, "Visual", { reverse = true })
-
-				vim.cmd([[set cursorline]])
-				vim.cmd([[set cursorcolumn]])
-				vim.cmd([[highlight clear SignColumn]])
-
 				local diagnostic_colors = {
 					Error = { fg = "#cc241d", reverse = true },
 					Warn = { fg = "#fabd2f" },
@@ -33,8 +59,7 @@ return {
 				-- for illuminate plugun
 				vim.api.nvim_set_hl(0, "IlluminatedWord", { bg = "#b47109" })
 				vim.api.nvim_set_hl(0, "IlluminatedWordRead", {
-					bg = "#b47109",
-					fg = "#d5c4a1",
+					bg = "#504945",
 				})
 				vim.api.nvim_set_hl(0, "IlluminatedWordWrite", {
 					bg = "#504945",
@@ -42,22 +67,12 @@ return {
 				vim.api.nvim_set_hl(0, "IlluminatedWordText", {
 					bg = "#504945",
 				})
-				vim.opt.termguicolors = true
-				-- vim.api.nvim_set_hl(0, "Normal", { bg = "#282828" })
+
+				vim.api.nvim_set_hl(0, "CursorLine", { bg = "#333852" })
+				vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#333852" })
 			end,
 		})
-		vim.g.gruvbox_italic_functions = true
-
-		vim.api.nvim_create_user_command("SetGruvboxFlatStyle", function(opts)
-			vim.g.gruvbox_flat_style = opts.args
-			vim.cmd("colorscheme gruvbox-flat")
-		end, {
-			nargs = 1,
-			complete = function(_, _, _)
-				return { "hard", "dark" }
-			end,
-		})
-
-		vim.cmd([[colorscheme gruvbox-flat]])
+		vim.cmd.colorscheme("gruvbox-material")
+		apply_italic_highlights()
 	end,
 }
