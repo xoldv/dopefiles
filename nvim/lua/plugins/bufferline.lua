4d385f287841ca3c4844f0904c2260b8ab612006
xoldv <the507015t@gmail.com>
2025-05-04 18:13:15 +0200

    feat(vim): add moving through buffers with cmd+num

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,79 +6,42 @@ return {
 		local bufferline = require("bufferline")
 		bufferline.setup({
 			options = {
-				mode = "buffers", -- set to "tabs" to only show tabpages instead
+				mode = "buffers",
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				always_show_bufferline = true,
+				diagnostics = "nvim_lsp",
+				show_buffer_icons = false,
+				numbers = "ordinal",
+				max_name_length = 10,
+				tab_size = 10,
 			},
 			highlights = {
 				buffer_selected = {
 					italic = true,
 					bold = true,
 				},
-				fill = {fg = "#282828", bg = "#282828"},
+				fill = { fg = "#282828", bg = "#181818" },
 			},
 		})
+
 		vim.opt.termguicolors = true
+
+		local function go_to_visual_buffer(n)
+			local buffers = bufferline.get_elements().elements
+			local target = buffers[n]
+			if target then
+				vim.api.nvim_set_current_buf(target.id)
+			end
+		end
+
+		for i = 1, 9 do
+			vim.keymap.set("n", "<A-" .. i .. ">", function()
+				go_to_visual_buffer(i)
+			end, { noremap = true, silent = true })
+		end
+		vim.keymap.set("n", "<A-0>", function()
+			go_to_visual_buffer(10)
+		end, { noremap = true, silent = true })
 	end,
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>1",
-		'<cmd>lua require("bufferline").go_to_buffer(1, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>2",
-		'<cmd>lua require("bufferline").go_to_buffer(2, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>3",
-		'<cmd>lua require("bufferline").go_to_buffer(3, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>4",
-		'<cmd>lua require("bufferline").go_to_buffer(4, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>5",
-		'<cmd>lua require("bufferline").go_to_buffer(5, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>6",
-		'<cmd>lua require("bufferline").go_to_buffer(6, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>7",
-		'<cmd>lua require("bufferline").go_to_buffer(7, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>8",
-		'<cmd>lua require("bufferline").go_to_buffer(8, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>9",
-		'<cmd>lua require("bufferline").go_to_buffer(9, true)<cr>',
-		{ noremap = true, silent = true }
-	),
-	vim.api.nvim_set_keymap(
-		"n",
-		"<LEADER>0",
-		'<cmd>lua require("bufferline").go_to_buffer(10, true)<cr>',
-		{ noremap = true, silent = true }
-	),
 }
