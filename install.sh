a89890ea1df00fb43729d8b6547ab17a9595366f
xoldv <the507015t@gmail.com>
2025-07-23 11:56:54 +0200

    change installer
    Add brew stuff, add gitstatus cfg, remove unused p10k and kitty

--- install.sh
+++ install.sh
@@ -13,26 +13,33 @@ function installing_func(){
 }
 
 function brew_installing(){
-    if command -v brew >/dev/null 
-    then 
+    if command -v brew >/dev/null; then
         echo -e "${BGreen}Brew already installed!"
+
     else
         echo -e "${Blue}Installing Brew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
         (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
         eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
     return
 }
 
+
 function oh_my_zsh_installing(){
     if [[ $ZSH = $(pwd)/.oh-my-zsh ]]; then
         echo -e "${BGreen}Oh-My-ZSH already installed!"
-    else
 
+    else
         echo -e "${Blue}Installing Oh-My-ZSH..."
         export RUNZSH=no
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+        echo -e "${Blue}Installing ZSH plugins..."
+        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/gitstatus
+
         # if [[ $SHELL = /bin/bash/ ]]; then
         #     chsh -s $(which zsh)
         # fi
@@ -42,26 +49,11 @@ function oh_my_zsh_installing(){
 
 }
 
-function powerlevel10k_installing(){
-    theme=$(grep -c "p10k" ~/.zshrc)
-    if [[ $theme -le 0 ]]; then
-        rm -rf ~/.p10k.zsh
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-        echo -e "${Blue}Installing ZSH plugins..."
-        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-    else
-        echo -e "${BGreen}Theme PowerLevel10k already isntalled!"
-
-    fi
-    return
-}
 
 function brew_check_status(){
     local installing="$1"
     local name="$2"
+
     if [[ $installing =~ "Not installed" ]]; then
         echo -e "${Blue}Installing $name..."
         brew install $name
@@ -73,13 +65,11 @@ function brew_check_status(){
 
 
 function brew_stuff_installing(){
+    stuff=("neovim" "lf" "tmux" "fastfetch" "alacritty" "lazygit" "fzf" "colima" "docker" "docker-compose" "ripgrep" "ruff" "pyright")
 
-    stuff=("neovim" "lf" "tmux" "eza" "fastfetch" "alacritty")
     for brew_service in ${stuff[@]}; do
         brew_check_status "$(brew info "${brew_service}")" "$brew_service"
     done
-    # kitty installing or updating
-    # curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
     return
 }
@@ -88,12 +78,15 @@ function brew_stuff_installing(){
 function backup_nvim(){
     while true; do
         read -p "Make a backup of your current nvim config (y/n)? " yn
+
         case $yn in
             [Yy]* ) mv ~/.config/nvim ~/.config/nvim.bak; mv ~/.local/share/nvim ~/.local/share/nvim.bak; mv ~/.local/state/nvim; ~/.local/state/nvim.bak mv ~/.cache/nvim ~/.cache/nvim.bak; break;;
             [Nn]* ) break;;
             * ) echo "Please answer yes or no.";;
         esac
+
     done
+
     if [[ $yn = n ]]; then
         rm -rf ~/.config/nvim
         rm -rf ~/.local/share/nvim
@@ -107,8 +100,8 @@ function backup_nvim(){
 function full_replacing_files(){
     cp -fr ~/dopefiles/ ~/.config/
     ln -s ~/.config/.zshrc ~/
+    ln -s ~/.config/gitstatus.prompt.zsh ~/gitstatus/
     ln -s ~/.config/.tmux.conf ~/
-    ln -s ~/.config/.p10k.zsh ~/
     return
 }
 
@@ -140,9 +133,11 @@ cat << "EOF"
 EOF
 while true; do
     read -p "Reload terminal with zsh (y/n)? " yn
+
     case $yn in
         [Yy]* ) exec zsh; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
+
 done
