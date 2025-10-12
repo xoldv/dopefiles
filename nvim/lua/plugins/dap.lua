9c81056d2a858e8619f5f9a1618336c5392ddcca
xoldv <the507015t@gmail.com>
2025-10-12 21:15:09 +0200

    feat(nvim): Remove autoclosing from DAP

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -11,57 +11,30 @@ return {
 		config = function()
 			local dap = require("dap")
 			local ui = require("dapui")
+			ui.setup()
 
-			require("dapui").setup()
-			require("dap-python").setup('/opt/homebrew/bin/python3')
+			local function close_debugger()
+				ui.close()
+				dap.terminate()
+			end
+			require("dap-python").setup("/opt/homebrew/bin/python3")
 
 			require("nvim-dap-virtual-text").setup({
 				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-				display_callback = function(variable)
-					local name = string.lower(variable.name)
-					local value = string.lower(variable.value)
-					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
-						return "*****"
-					end
-
-					if #variable.value > 15 then
-						return " " .. string.sub(variable.value, 1, 15) .. "... "
-					end
-
-					return " " .. variable.value
-				end,
+				-- display_callback = function(variable)
+				-- 	local name = string.lower(variable.name)
+				-- 	local value = string.lower(variable.value)
+				-- 	if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+				-- 		return "*****"
+				-- 	end
+				--
+				-- 	if #variable.value > 15 then
+				-- 		return " " .. string.sub(variable.value, 1, 15) .. "... "
+				-- 	end
+				--
+				-- 	return " " .. variable.value
+				-- end,
 			})
-
-			-- Handled by nvim-dap-go
-			-- dap.adapters.go = {
-			--   type = "server",
-			--   port = "${port}",
-			--   executable = {
-			--     command = "dlv",
-			--     args = { "dap", "-l", "127.0.0.1:${port}" },
-			--   },
-			-- }
-
-			local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
-			if elixir_ls_debugger ~= "" then
-				dap.adapters.mix_task = {
-					type = "executable",
-					command = elixir_ls_debugger,
-				}
-
-				dap.configurations.elixir = {
-					{
-						type = "mix_task",
-						name = "phoenix server",
-						task = "phx.server",
-						request = "launch",
-						projectDir = "${workspaceFolder}",
-						exitAfterTaskReturns = false,
-						debugAutoInterpretAllModules = false,
-					},
-				}
-			end
-
 			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
 
@@ -77,6 +50,8 @@ return {
 			vim.keymap.set("n", "<F5>", dap.step_back)
 			vim.keymap.set("n", "<F13>", dap.restart)
 
+			vim.keymap.set("n", "<leader>dq", close_debugger, { desc = "Close DAP UI and terminate debug session" })
+
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
 			end
@@ -84,10 +59,10 @@ return {
 				ui.open()
 			end
 			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
+				-- ui.close()
 			end
 			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
+				-- ui.close()
 			end
 		end,
 	},
