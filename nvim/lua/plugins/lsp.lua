11f3dc0e83c34987efe2c38df2bf8daaaa4ae313
xoldv <the507015t@gmail.com>
2025-10-12 19:39:40 +0200

    feat(nvim): Refactor lsp for nvim v0.11.2

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,38 +8,13 @@ return {
 		{ "SmiteshP/nvim-navic" },
 		{ "jay-babu/mason-null-ls.nvim" },
 		{ "nvimtools/none-ls.nvim" },
-		{ "stevearc/conform.nvim" },
+		{ "nvimtools/none-ls-extras.nvim" },
 	},
 
 	config = function()
 		-- Настройка Mason
 		require("mason").setup()
 
-		-- Настройка Mason LSPConfig
-		-- не ворк после обновы
-		-- require("mason-lspconfig").setup({
-		-- 	ensure_installed = {
-		-- 		"emmet_language_server", -- Для Emmet
-		-- 		"lua_ls", -- Для Lua
-		-- 		"pyright", -- Для Python
-		-- 		"clangd", -- Для C/C++
-		-- 	},
-		-- })
-
-		-- Настройка каждого LSP-сервера
-		local lspconfig = require("lspconfig")
-
-		-- Логика подключения серверов
-		lspconfig.lua_ls.setup({
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
-		lspconfig.pyright.setup({
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
-		lspconfig.emmet_language_server.setup({})
-		lspconfig.clangd.setup({})
-		lspconfig.ts_ls.setup({})
-		lspconfig.intelephense.setup({})
 		require("mason-null-ls").setup({
 			ensure_installed = { "ruff", "stylua", "clang_format", "prettier" },
 		})
@@ -48,32 +23,20 @@ return {
 
 		null_ls.setup({
 			sources = {
-				-- null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.pretty_php,
 				null_ls.builtins.formatting.prettier.with({
 					extra_args = { "--tab-width", "4" },
 				}),
+				require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+				require("none-ls.formatting.ruff_format"),
 			},
 			on_attach = function(client, bufnr)
-				-- Привязка кнопки для форматирования
 				vim.keymap.set("n", "<leader>m", function()
 					vim.lsp.buf.format({ async = true })
 				end, { noremap = true, silent = true, buffer = bufnr, desc = "Format Code" })
 			end,
 		})
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				python = {
-					"ruff_fix", -- To fix lint errors. (ruff with argument --fix)
-					"ruff_format", -- To run the formatter. (ruff with argument format)
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>m", function()
-			conform.format()
-		end, { noremap = true, silent = true, desc = "Conform Format" })
 	end,
 }
