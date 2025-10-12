5196e24b4756b79d57989f25c767eff243673eb6
xoldv <the507015t@gmail.com>
2025-10-12 19:40:54 +0200

    feat(nvim): Add vim-surround and keybind for open git blame in vim-fugitive

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,9 +1,23 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-    config = function ()
-        vim.keymap.set("n", "<leader>ga", ":Git blame<CR>")
-    end,
-  }
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+		config = function()
+			local function toggle_git_blame()
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					local buf_ft = vim.api.nvim_buf_get_option(buf, "filetype")
+
+					if buf_ft == "fugitiveblame" then
+						vim.api.nvim_win_close(win, false)
+						return
+					end
+				end
+
+				vim.cmd("Git blame")
+			end
+
+			vim.keymap.set("n", "ga", toggle_git_blame, { desc = "Toggle git blame" })
+		end,
+	},
 }
