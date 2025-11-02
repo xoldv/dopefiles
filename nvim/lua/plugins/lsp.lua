1ac4366a6d70814b4271baf453f4fa15e46558e9
xoldv <the507015t@gmail.com>
2025-11-02 17:56:53 +0200

    feat(nvim): Use conform instead of null ls and add nvim-bqf

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
 	dependencies = {
 		"mason-org/mason.nvim",
 		"mason-org/mason-lspconfig.nvim",
-		"jay-babu/mason-null-ls.nvim",
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
@@ -12,21 +11,21 @@ return {
 		"hrsh7th/cmp-cmdline",
 		-- "L3MON4D3/LuaSnip",
 		-- "saadparwaiz1/cmp_luasnip",
-		"nvimtools/none-ls.nvim",
-		"nvimtools/none-ls-extras.nvim",
 		"j-hui/fidget.nvim",
 		"hrsh7th/cmp-vsnip",
 		"hrsh7th/vim-vsnip",
+		"stevearc/conform.nvim",
 	},
 
 	config = function()
 		require("mason").setup()
-		require("mason-null-ls").setup({
-			ensure_installed = { "ruff", "stylua", "clang_format", "prettier" },
-		})
 		require("fidget").setup()
-
-		local null_ls = require("null-ls")
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+			},
+		})
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local cmp = require("cmp")
 		cmp.setup({
@@ -88,23 +87,6 @@ return {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 		})
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.pretty_php,
-				null_ls.builtins.formatting.prettier.with({
-					extra_args = { "--tab-width", "4" },
-				}),
-				require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
-				require("none-ls.formatting.ruff_format"),
-			},
-			on_attach = function(client, bufnr)
-				vim.keymap.set("n", "<leader>m", function()
-					vim.lsp.buf.format({ async = true })
-				end, { noremap = true, silent = true, buffer = bufnr, desc = "Format Code" })
-			end,
-		})
 
 		vim.lsp.config("lua_ls", { capabilities = capabilities })
 		vim.lsp.config("basedpyright", { capabilities = capabilities })
@@ -114,11 +96,13 @@ return {
 
 		vim.lsp.enable("lua_ls")
 		vim.lsp.enable("basedpyright")
-		-- vim.lsp.enable("pyright")
 		vim.lsp.enable("emmet_language_server")
 		vim.lsp.enable("clangd")
 
 		vim.keymap.set("n", "<leader>fu", vim.lsp.buf.references, { desc = "Find Usages" })
 		vim.keymap.set("n", "<A-Enter>", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+		vim.keymap.set("n", "<leader>m", function()
+			require("conform").format({ async = true })
+		end)
 	end,
 }
