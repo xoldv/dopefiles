0aa29f4e3a0b86145fe32423903516e980e12365
xoldv <the507015t@gmail.com>
2025-11-11 21:15:06 +0200

    feat(nvim-color): Try using IlyasYOY's theme

---
+++ nvim/lua/plugins/color.lua
@@ -0,0 +1,61 @@
+local function set_italic(group)
+	local current = vim.api.nvim_get_hl(0, { name = group, link = false }) or {}
+	current.italic = true
+	vim.api.nvim_set_hl(0, group, current)
+end
+
+local function apply_italic_highlights()
+	set_italic("@keyword")
+	set_italic("@function.method.call")
+	set_italic("@function.call")
+end
+
+return {
+
+	"IlyasYOY/theme.nvim",
+	dependencies = { "tjdevries/colorbuddy.nvim" },
+
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "ilyasyoy",
+			callback = function()
+				local diagnostic_colors = {
+					Error = { fg = "#5b222c" },
+					Warn = { fg = "#7a4d06" },
+					Info = { fg = "#458588" },
+					Hint = { fg = "#689d6a" },
+				}
+				for type, color in pairs(diagnostic_colors) do
+					vim.api.nvim_set_hl(0, "DiagnosticVirtualText" .. type, {
+						fg = color.fg,
+					})
+				end
+
+				for type, color in pairs(diagnostic_colors) do
+					vim.api.nvim_set_hl(0, "DiagnosticSign" .. type, {
+						fg = color.fg,
+					})
+				end
+				-- brackets
+				vim.api.nvim_set_hl(0, "MatchParen", { fg = "#fc9487", bg = "#cc241d", bold = true })
+				-- for illuminate plugun
+				vim.api.nvim_set_hl(0, "IlluminatedWord", { bg = "#b47109" })
+				vim.api.nvim_set_hl(0, "IlluminatedWordRead", {
+					bg = "#454545",
+				})
+				vim.api.nvim_set_hl(0, "IlluminatedWordWrite", {
+					bg = "#454545",
+				})
+				vim.api.nvim_set_hl(0, "IlluminatedWordText", {
+					bg = "#454545",
+				})
+
+				vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#2a2a2a" }) -- фон
+			end,
+		})
+		vim.cmd.colorscheme("ilyasyoy")
+		apply_italic_highlights()
+	end,
+}
