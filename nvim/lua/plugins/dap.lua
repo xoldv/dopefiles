c7db774f1820df355256a6b59094cfeddff40ab6
xoldv <cuztrippie666@gmail.com>
2025-12-16 23:58:23 +0200

    feat(nvim): Refactor plugins.dap and remove non-working reloading before start tests

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -9,43 +9,28 @@ return {
 			"Weissle/persistent-breakpoints.nvim",
 		},
 		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			dapui.setup()
+
+			local function close_debugger()
+				dapui.close()
+				dap.terminate()
+			end
+
+			require("dap-python").setup("/opt/homebrew/bin/python3")
+			require("nvim-dap-virtual-text").setup()
 			require("persistent-breakpoints").setup({
 				load_breakpoints_event = { "BufReadPost" },
 			})
 
-			require("persistent-breakpoints.api").load_breakpoints()
-			local dap = require("dap")
-			local ui = require("dapui")
-			ui.setup()
-
-			local function close_debugger()
-				ui.close()
-				dap.terminate()
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
 			end
-			require("dap-python").setup("/opt/homebrew/bin/python3")
-
-			require("nvim-dap-virtual-text").setup({
-				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-				-- display_callback = function(variable)
-				-- 	local name = string.lower(variable.name)
-				-- 	local value = string.lower(variable.value)
-				-- 	if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
-				-- 		return "*****"
-				-- 	end
-				--
-				-- 	if #variable.value > 15 then
-				-- 		return " " .. string.sub(variable.value, 1, 15) .. "... "
-				-- 	end
-				--
-				-- 	return " " .. variable.value
-				-- end,
-			})
-			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
-
-			-- Eval var under cursor
-			vim.keymap.set("n", "<leader>?", function()
-				require("dapui").eval(nil, { enter = true })
-			end)
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
 
 			vim.keymap.set("n", "<F1>", dap.continue)
 			vim.keymap.set("n", "<F2>", dap.step_into)
@@ -54,26 +39,13 @@ return {
 			vim.keymap.set("n", "<F5>", dap.step_back)
 			vim.keymap.set("n", "<F6>", dap.restart)
 
+			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+			vim.keymap.set("n", "<leader>?", function()
+				dapui.eval(nil, { enter = true })
+			end)
 			vim.keymap.set("n", "<leader>dq", close_debugger, { desc = "Close DAP UI and terminate debug session" })
-
-			vim.keymap.set(
-				"n",
-				"<leader>b",
-				"<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
-				{ noremap = true, silent = true }
-			)
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				-- ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				-- ui.close()
-			end
+			vim.keymap.set("n", "<leader>b", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>")
+			vim.keymap.set("n", "<leader>B", "<cmd>lua require('persistent-breakpoints.api').reload_breakpoints()<cr>")
 		end,
 	},
 }
