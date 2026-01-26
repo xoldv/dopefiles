11ba9ba3cc05dbfc4ff71525bd041dc879e5d14c
xoldv <the507015t@gmail.com>
2026-01-26 21:21:55 +0200

    feat(nvim:harpoon): Use Command MacOS key in harpoon keybindings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -12,20 +12,27 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
-		vim.keymap.set('n', '<C-e>', function()
+
+        -- <Esc> - command toggle on macos
+        -- alacritty.toml
+        -- [[keyboard.bindings]]
+        -- key = "e"
+        -- mods = "Command"
+        -- chars = "\u001be"
+		vim.keymap.set('n', '<Esc>e', function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<C-1>", function()
+		vim.keymap.set("n", "<Esc>1", function()
 			harpoon:list():select(1)
 		end)
-		vim.keymap.set("n", "<C-2>", function()
+		vim.keymap.set("n", "<Esc>2", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<C-3>", function()
+		vim.keymap.set("n", "<Esc>3", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<C-4>", function()
+		vim.keymap.set("n", "<Esc>4", function()
 			harpoon:list():select(4)
 		end)
 
