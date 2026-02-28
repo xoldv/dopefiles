1b13f5123917ad2502dcc3422d66f9ccfd78e1bd
xoldv <the507015t@gmail.com>
2026-02-28 15:27:28 +0200

    feat(nvim): Add M-<hjkl> keybindings for change window size

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,6 +20,10 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = tru
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+vim.keymap.set("n", "<M-l>", "<C-w>5>")
+vim.keymap.set("n", "<M-h>", "<C-w>5<")
+vim.keymap.set("n", "<M-k>", "<C-w>-")
+vim.keymap.set("n", "<M-j>", "<C-w>+")
 
 -- Go to declaration
 vim.keymap.set("n", "gd", "<C-]>", { noremap = true, silent = true })
