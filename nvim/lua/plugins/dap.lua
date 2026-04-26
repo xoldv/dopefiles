bc7424144eb5aa6cbc229265bea9020697624e32
xoldv <the507015t@gmail.com>
2026-04-26 18:49:36 +0200

    feat(nvim): Migrate nvim-dap-view -> debugmaster.nvim

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,16 +2,15 @@ vim.pack.add({
 	{ src = "https://github.com/mfussenegger/nvim-dap" },
 	{ src = "https://github.com/mfussenegger/nvim-dap-python" },
 	{ src = "https://github.com/leoluz/nvim-dap-go" },
-	{ src = "https://github.com/igorlfs/nvim-dap-view" },
+	{ src = "https://github.com/MironPascalCaseFan/debugmaster.nvim" },
 	{ src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
 	{ src = "https://github.com/Weissle/persistent-breakpoints.nvim" },
 })
 
 local dap = require("dap")
-local dapview = require("dap-view")
+local debugmaster = require("debugmaster")
 local persistent_breakpoints = require("persistent-breakpoints.api")
 
-dapview.setup()
 require("dap-python").setup("/opt/homebrew/bin/python3")
 require("dap-go").setup()
 require("nvim-dap-virtual-text").setup()
@@ -19,10 +18,10 @@ require("persistent-breakpoints").setup({
 	load_breakpoints_event = { "BufReadPost" },
 })
 
+dap.listeners.before.attach.dapui_config = debugmaster.mode.toggle
+dap.listeners.before.launch.dapui_config = debugmaster.mode.toggle
 
-dap.listeners.before.attach.dapui_config = dapview.open
-dap.listeners.before.launch.dapui_config = dapview.open
-
+vim.keymap.set({ "n", "v" }, "<leader>z", debugmaster.mode.toggle, { nowait = true })
 vim.keymap.set("n", "<leader>bb", persistent_breakpoints.toggle_breakpoint)
 
 vim.keymap.set("n", "<leader>bc", persistent_breakpoints.clear_all_breakpoints)
@@ -36,9 +35,9 @@ vim.keymap.set("n", "<F6>", dap.restart)
 
 vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
 vim.keymap.set("n", "<leader>?", function()
-	dapview.eval(nil, { enter = true })
+	debugmaster.eval(nil, { enter = true })
 end)
 vim.keymap.set("n", "<leader>dq", function()
-	dapview.close()
+	debugmaster.mode.disable()
 	dap.terminate()
 end, { desc = "Close debugger" })
